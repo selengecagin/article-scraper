a632523f2e1aabba18fca99a84b779f1cad1c7db
selengecagin <selengecagin@gmail.com>
2024-07-25 21:35:23 +0300

    Create JSON file for scraped data

--- main.py
+++ main.py
@@ -9,29 +9,29 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 try:
-    # Navigate to Google Turkey
-    driver.get("https://www.google.com.tr")
+    # Navigate to "https://code-maze.com/"
+    driver.get("https://code-maze.com/latest-posts-on-code-maze/")
 
-    # Wait for the page to load
-    time.sleep(3)
-
-    # Find the search box
-    search_box = driver.find_element(By.NAME, "q")
-
-    # Wait before typing
+    # Wait 2 seconds
     time.sleep(2)
 
-    # Type "Hello World!" in the search box
-    search_box.send_keys("Hello World!")
+    # Find <h2> elements with class "entry-title"
+    titles = driver.find_elements(By.CSS_SELECTOR, "h2.entry-title")
 
-    # Wait before pressing Enter
+    # get the href value of the first <a> element in the <h2> element
+    for title in titles:
+        # Print the text of the <a> element
+        print(title.find_element(By.CSS_SELECTOR, "a").text)
+
+        # Print the href attribute of the <a> element
+        print(title.find_element(By.CSS_SELECTOR, "a").get_attribute("href"))
+
+        # Save the title and link to a json file
+        with open("titles.json", "a") as f:
+            f.write(f'{{"title": "{title.find_element(By.CSS_SELECTOR, "a").text}", "link": "{title.find_element(By.CSS_SELECTOR, "a").get_attribute("href")}"}}\n')
+
+    # Wait 2 seconds
     time.sleep(2)
-
-    # Press Enter to perform the search
-    search_box.send_keys(Keys.RETURN)
-
-    # Wait for the search results to load
-    time.sleep(5)
 
 finally:
     # Close the browser after a short delay to see the result
